b21aa98389aeb45173c892c2af6acd14d0ce0112
JosefineNorden <iorbanderas@gmail.com>
2025-06-25 13:34:21 +0200

    Add project files.

---
+++ Data/PadelBookingContext.cs
@@ -0,0 +1,16 @@
+﻿using Enskilt_Projektarbete.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Enskilt_Projektarbete.Data
+{
+    public class PadelBookingContext : DbContext
+    {
+        public PadelBookingContext(DbContextOptions<PadelBookingContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers => Set<Customer>();
+        public DbSet<Booking> Bookings => Set<Booking>();
+    }
+}
---
+++ Models/Booking.cs
@@ -0,0 +1,12 @@
+﻿namespace Enskilt_Projektarbete.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; } = null!;
+        public int CourtNumber { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
